1fc504ccd8831076052f2ace9ca72571189aca3b
kichu1990 <34007976+kichu1990@users.noreply.github.com>
2018-04-16 17:54:26 +0100

    Update Internet Privacy Poll.R

--- Internet Privacy Poll.R
+++ Internet Privacy Poll.R
@@ -2,11 +2,9 @@ setwd("D:\\Study  Materials\\R Language\\DataSet")
 IPS=read.csv("AnonymityPoll.csv")
 
 #How many people participated in the poll?
-
 nrow(IPS) #1002
 
 #How many interviewees responded that they use a smartphone?
-
 table(IPS$Smartphone) #487
 
 #How many interviewees responded that they don't use a smartphone?
@@ -18,18 +16,14 @@ summary(IPS)
 sum(is.na(IPS))#43
 
 #Which of the following are states in the Midwest census region? 
-
 unique(IPS$State[which(IPS$Region=="Midwest")])
 
 #Wich was the state in the South census region with the largest number of interviewees?
-
 table(IPS$State,IPS$Region)
-
 unique(IPS$Region)
 
 #How many interviewees reported not having used the Internet and not having used a 
 #smartphone?
-
 table(IPS$Internet.Use,IPS$Smartphone)
 #186
 
@@ -50,24 +44,19 @@ sum(is.na(IPS$Smartphone))#43
 
 #Use the subset function to obtain a data frame called "limited", which is limited to 
 #interviewees who reported Internet use or who reported smartphone use.
-
 limited_IPS=subset(IPS,Smartphone==1|Internet.Use==1)
 
 #How many interviewees are in the new data frame?
-
 nrow(limited_IPS)
 
 #Which variables have missing values in the limited data frame?
-
 summary(limited_IPS)
 
 #What is the average number of pieces of personal information on the Internet, 
-#according to the Info.On.Internet variable?
-
+#according to the Info.On.Internet variable
 mean(limited_IPS$Info.On.Internet)
 
 #How many interviewees reported a value of 0 for Info.On.Internet?
-
 table(limited_IPS$Info.On.Internet) #105
 
 #How many interviewees reported the maximum value of 11 for Info.On.Internet?
@@ -80,49 +69,37 @@ table(limited_IPS$Worry.About.Info)
 386/(386+404)
 
 #What proportion of interviewees who answered the Anonymity.
-
 table(limited_IPS$Anonymity.Possible)
 278/(475+278)
 
 #What proportion of interviewees who answered the Tried.
-
 table(limited_IPS$Tried.Masking.Identity)
 128/(128+656)
 
 #What proportion of interviewees who answered the Privacy.Laws
-
 table(limited_IPS$Privacy.Laws.Effective)
 186/(186+541)
 
 #Build a histogram of the age of interviewees. What is the best represented age group 
 #in the population?
-
-hist(limited_IPS$Age,breaks=100)
-
+hist(limited_IPS$Age,breaks=100
 plot(limited_IPS$Age,limited_IPS$Info.On.Internet)
-
 max(table(limited_IPS$Age,limited_IPS$Info.On.Internet))
 
 #To avoid points covering each other up, we can use the jitter() function on the values 
 #we pass to the plot function. Experimenting with the command jitter(c(1, 2, 3)), 
 #what appears to be the functionality of the jitter command?
-
 jitter(c(1, 2, 3))
 
 #jitter adds or subtracts a small amount of random noise to the values passed to it, and two runs will yield different 
 #results
-
 #Now, plot Age against Info.On.Internet with plot(jitter(limited$Age), jitter(limited$Info.On.Internet)). 
 #What relationship to you observe between Age and Info.On.Internet?
-
 plot(jitter(limited_IPS$Age), jitter(limited_IPS$Info.On.Internet))
-
 #Older age seems moderately associated with a smaller value for Info.On.Internet
 
 #What is the average Info.On.Internet value for smartphone users?
-
 tapply(limited_IPS$Info.On.Internet,limited_IPS$Smartphone,summary)
-
 #2.92
 
 #What is the average Info.On.Internet value for non-smartphone users?
@@ -130,12 +107,9 @@ tapply(limited_IPS$Info.On.Internet,limited_IPS$Smartphone,summary)
 
 #What proportion of smartphone users who answered the Tried.Masking.Identity question have 
 #tried masking their identity when using the Internet?
-
 tapply(limited_IPS$Tried.Masking.Identity,limited_IPS$Smartphone,table)
-
 93/(93+390)
 
 #What proportion of non smartphone users who answered the Tried.Masking.
 #Identity question have tried masking their identity when using the Internet?
-
 33/(248+33)
